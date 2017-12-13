69e723f74798f911f723a781e196e88596f56205
Tyler <twr349@gmail.com>
2017-12-13 21:22:25 +0000

    get_me defined

--- kele.gemspec
+++ kele.gemspec
@@ -11,5 +11,5 @@ Gem::Specification.new do |s|
    s.homepage      =
      'http://rubygems.org/gems/kele'
    s.license       = 'MIT'
-   s.add_runtime_dependency 'httparty', '~> 0.13'
+   s.add_runtime_dependency 'httparty', '0.9.0', 'json', '~> 1.8'
  end
--- lib/kele.rb
+++ lib/kele.rb
@@ -1,16 +1,24 @@
 require 'httparty'
-#require 'rubygems'
+require 'rubygems' 
+require 'json'
 
 class Kele
     include HTTParty
     
+    #credentials = @cred
     
     def initialize(email, password)
-        @auth = {query: { email: email, password: password }}
-        puts self.class.post('https://www.bloc.io/api/v1/sessions', @auth)
-    
+        @cred = {query: { email: email, password: password }}
+        response = self.class.post('https://www.bloc.io/api/v1/sessions', @cred)
+        @auth_token = response["auth_token"]
+        puts response
     end
 
+    def get_me
+        response = self.class.get('https://www.bloc.io/api/v1/users/me', headers: { "authorization" => @auth_token })
+        json_parse = JSON.parse(response.body)
+        puts json_parse
+    end
 
 
 end#end of ends
