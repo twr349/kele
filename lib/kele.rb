664b23e31a4fc6650a71f8f8b09952e2e5486644
Tyler <twr349@gmail.com>
2017-12-14 17:53:57 +0000

    story 4 completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -4,13 +4,14 @@ require 'json'
 
 class Kele
     include HTTParty
+    #format :json
     
-    #credentials = @cred
+    #@cred = credentials, email & password
     
     def initialize(email, password)
         @cred = {query: { email: email, password: password }}
         response = self.class.post('https://www.bloc.io/api/v1/sessions', @cred)
-        @auth_token = response["auth_token"]
+        @auth_token = response["auth_token"] #holds the authorization token
         puts response
     end
 
@@ -19,7 +20,14 @@ class Kele
         json_parse = JSON.parse(response.body)
         puts json_parse
     end
+    
+    def get_mentor_availability(mentor_id)
+        #Henry's mentor id = 2365811 
+        response = self.class.get("https://www.bloc.io/api/v1/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token })
+        mentor_availability = JSON.parse(response.body)
+        puts mentor_availability
+    end
+
 
 
 end#end of ends
-
