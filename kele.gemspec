5deba16d0ad7178eb3cd5a07f7390d4105e972e4
Tyler <twr349@gmail.com>
2017-12-14 19:54:57 +0000

    story-5 completed

--- kele.gemspec
+++ kele.gemspec
@@ -6,7 +6,7 @@ Gem::Specification.new do |s|
    s.description   = 'A client for the Bloc API'
    s.authors       = ['Tyler Roberts']
    s.email         = 'twr349@gmail.com'
-   s.files         = ['lib/kele.rb']
+   s.files         = ['lib/kele.rb', 'lib/kele/roadmap.rb']
    s.require_paths = ["lib"]
    s.homepage      =
      'http://rubygems.org/gems/kele'
--- lib/kele.rb
+++ lib/kele.rb
@@ -1,9 +1,11 @@
 require 'httparty'
 require 'rubygems' 
 require 'json'
+require_relative 'kele/roadmap'
 
 class Kele
     include HTTParty
+    include Roadmap
     #format :json
     
     #@cred = credentials, email & password
@@ -27,7 +29,19 @@ class Kele
         mentor_availability = JSON.parse(response.body)
         puts mentor_availability
     end
-
+    
+    #def get_roadmap(roadmap_id)
+        #roadmap_id =>38
+     #   response = self.class.get("https://www.bloc.io/api/v1/roadmaps/#{roadmap_id}", headers: { "authorization" => @auth_token })
+      #  roadmap = JSON.parse(response.body)
+       # puts roadmap
+    #end
+    
+    #def get_checkpoint(checkpoint_id)
+     #   response = self.class.get("https://www.bloc.io/api/v1/checkpoints/#{checkpoint_id}", headers: { "authorization" => @auth_token })
+      #  checkpoint = JSON.parse(response.body)
+       # puts checkpoint
+    #end
 
 
 end#end of ends
