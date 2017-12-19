890be0524ce63cfdafb652ffd1f78b03950db696
Tyler <twr349@gmail.com>
2017-12-19 20:29:33 +0000

    Messages can be sent

--- lib/kele/messages.rb
+++ lib/kele/messages.rb
@@ -6,12 +6,18 @@ module Messages
         if page > 0
            response = self.class.get("https://www.bloc.io/api/v1/message_threads?page=#{page}", headers: { "authorization" => @auth_token})
         else
-            response = self.class.get("https://www.bloc.io/api/v1/message_threads", headers: { "authorization" => @auth_token })
+           response = self.class.get("https://www.bloc.io/api/v1/message_threads", headers: { "authorization" => @auth_token })
         end
       message = JSON.parse(response.body)
      puts message
     end
- 
+    
+    def create_message(sender, recipient_id, subject, stripped_text)
+        response =self.class.post("https://www.bloc.io/api/v1/messages", body: { "sender" => sender, "recipient_id" => recipient_id, "subject" => subject, "stripped_text" => stripped_text }, headers: { "authorization" => @auth_token })
+        if response.success?
+            puts "message sent!"
+        end
+    end
 
 
 
